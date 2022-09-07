2b86062d1ff912443c05d67553f620b0f3147324
parasviitorcloud <paras.modi@viitor.cloud>
2022-09-07 16:51:15 +0530

    second commit

---
+++ DemoProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoProject
+//
+//  Created by VC-MacMini on 06/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Demo Project")
+    }
+
+
+}
+
