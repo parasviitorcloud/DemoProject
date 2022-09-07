251978f8c71355deaae106e165a62e8ebe99358e
parasviitorcloud <paras.modi@viitor.cloud>
2022-09-07 17:09:56 +0530

    addSum function

--- DemoProject/ViewController.swift
+++ DemoProject/ViewController.swift
@@ -14,10 +14,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("Demo Project")
         sum()
+        addSum()
     }
 
     // MARK: - Sum
     func sum() {
+        let a = "PA"
+        let b = "RAS"
+        let c = a + b
+        print("c= \(c)")
+    }
+    
+    // MARK: - AddSum
+    func addSum() {
         let a = "PA"
         let b = "RAS"
         let c = a + b
